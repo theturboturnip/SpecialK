e2578af46aa9208d5d2c0882a991f7719b19cb50
Samuel Stark <samuel.winslow.stark@gmail.com>
2022-05-24 21:12:56 +0100

    Fixes for LCU hooks

--- lcu.cpp
+++ lcu.cpp
@@ -40,21 +40,21 @@ SK_SetPluginName(std::wstring name);
 // ID3D11Device::CreateTexture2D - has a SpecialK override
 // The pointer to the original function.
 // A variable with this name is already declared (extern) in dxgi_backend.h and instantiated in d3d11.cpp, so make it (static) here.
-static D3D11Dev_CreateTexture2D_pfn           D3D11Dev_CreateTexture2D_Original = nullptr;
+static D3D11Dev_CreateTexture2D_pfn           _D3D11Dev_CreateTexture2D_Original = nullptr;
 
 // CreateTexture2D already has an override defined in d3d11.cpp, for texture cacheing.
-// We have to set our hook so it goes LCU_CreateTexture2D -> D3D11Dev_CreateTexture2D_Override -> D3D11Dev_CreateTexture2D_Original
+// We have to set our hook so it goes LCU_CreateTexture2D -> D3D11Dev_CreateTexture2D_Override -> _D3D11Dev_CreateTexture2D_Original
 // => we have to know where the middle override is.
 extern HRESULT
 WINAPI
 D3D11Dev_CreateTexture2D_Override(
     _In_            ID3D11Device* This,
-    _In_  /*const*/ D3D11_TEXTURE2D_DESC* pDesc,
+    _In_      const D3D11_TEXTURE2D_DESC* pDesc,
     _In_opt_  const D3D11_SUBRESOURCE_DATA* pInitialData,
     _Out_opt_       ID3D11Texture2D** ppTexture2D);
 
 // ID3D11Device::CreateRenderTargetView
-static D3D11Dev_CreateRenderTargetView_pfn D3D11Dev_CreateRenderTargetView_Original = nullptr;
+static D3D11Dev_CreateRenderTargetView_pfn _D3D11Dev_CreateRenderTargetView_Original = nullptr;
 
 extern HRESULT
 STDMETHODCALLTYPE
@@ -65,7 +65,7 @@ D3D11Dev_CreateRenderTargetView_Override(
     _Out_opt_       ID3D11RenderTargetView** ppRTView);
 
 // ID3D11Device::CreateDepthStencilView
-static D3D11Dev_CreateDepthStencilView_pfn D3D11Dev_CreateDepthStencilView_Original = nullptr;
+static D3D11Dev_CreateDepthStencilView_pfn _D3D11Dev_CreateDepthStencilView_Original = nullptr;
 
 extern HRESULT
 STDMETHODCALLTYPE
@@ -76,7 +76,7 @@ D3D11Dev_CreateDepthStencilView_Override(
     _Out_opt_       ID3D11DepthStencilView** ppRTView);
 
 // ID3D11DeviceContext::OMSetRenderTargets
-static D3D11_OMSetRenderTargets_pfn D3D11_OMSetRenderTargets_Original = nullptr;
+static D3D11_OMSetRenderTargets_pfn _D3D11_OMSetRenderTargets_Original = nullptr;
 
 extern void
 STDMETHODCALLTYPE
@@ -87,7 +87,7 @@ D3D11_OMSetRenderTargets_Override(
     _In_opt_ ID3D11DepthStencilView* pDepthStencilView);
 
 // ID3D11DeviceContext::RSSetViewports
-static D3D11_RSSetViewports_pfn D3D11_RSSetViewports_Original = nullptr;
+static D3D11_RSSetViewports_pfn _D3D11_RSSetViewports_Original = nullptr;
 
 extern void
 STDMETHODCALLTYPE
@@ -97,7 +97,7 @@ D3D11_RSSetViewports_Override(
     const D3D11_VIEWPORT* pViewports);
 
 // ID3D11DeviceContext::RSSetScissorRects (RenderDoc calls it RSSetScissors?)
-static D3D11_RSSetScissorRects_pfn D3D11_RSSetScissorRects_Original = nullptr;
+static D3D11_RSSetScissorRects_pfn _D3D11_RSSetScissorRects_Original = nullptr;
 
 extern void
 STDMETHODCALLTYPE
@@ -119,7 +119,7 @@ SK_LCU_CreateTexture2D(
     _Out_opt_       ID3D11Texture2D** ppTexture2D)
 {
     if (ppTexture2D == nullptr)
-        return D3D11Dev_CreateTexture2D_Original(This, pDesc, pInitialData, nullptr);
+        return _D3D11Dev_CreateTexture2D_Original(This, pDesc, pInitialData, nullptr);
 
     // Define this outside the switch scope so we can use it after the switch completes.
     D3D11_TEXTURE2D_DESC copy = *pDesc;
@@ -153,7 +153,7 @@ SK_LCU_CreateTexture2D(
     } break;
     }
 
-    HRESULT hr = D3D11Dev_CreateTexture2D_Original(This,
+    HRESULT hr = _D3D11Dev_CreateTexture2D_Original(This,
         pDesc, pInitialData,
         ppTexture2D);
 
@@ -169,7 +169,7 @@ SK_LCU_CreateRenderTargetView(
     _In_opt_  const D3D11_RENDER_TARGET_VIEW_DESC* pDesc,
     _Out_opt_       ID3D11RenderTargetView** ppRTView)
 {
-    return D3D11Dev_CreateRenderTargetView_Original(
+    return _D3D11Dev_CreateRenderTargetView_Original(
         This, pResource,
         pDesc, ppRTView);
 }
@@ -183,7 +183,7 @@ SK_LCU_CreateDepthStencilView(
     _In_opt_  const D3D11_DEPTH_STENCIL_VIEW_DESC* pDesc,
     _Out_opt_       ID3D11DepthStencilView** ppRTView)
 {
-    return D3D11Dev_CreateDepthStencilView_Original(
+    return _D3D11Dev_CreateDepthStencilView_Original(
         This, pResource,
         pDesc, ppRTView);
 }
@@ -197,7 +197,7 @@ SK_LCU_OMSetRenderTargets(
     _In_opt_ ID3D11RenderTargetView* const* ppRenderTargetViews,
     _In_opt_ ID3D11DepthStencilView* pDepthStencilView)
 {
-    D3D11_OMSetRenderTargets_Original(This, NumViews,
+    _D3D11_OMSetRenderTargets_Original(This, NumViews,
         ppRenderTargetViews, pDepthStencilView);
 }
 
@@ -209,7 +209,7 @@ SK_LCU_RSSetViewports(
     UINT                 NumViewports,
     const D3D11_VIEWPORT* pViewports)
 {
-    return D3D11_RSSetViewports_Original(This, NumViewports, pViewports);
+    return _D3D11_RSSetViewports_Original(This, NumViewports, pViewports);
 }
 
 __declspec (noinline)
@@ -220,7 +220,7 @@ SK_LCU_RSSetScissorRects(
     UINT                 NumRects,
     const D3D11_RECT* pRects)
 {
-    return D3D11_RSSetScissorRects_Original(This, NumRects, pRects);
+    return _D3D11_RSSetScissorRects_Original(This, NumRects, pRects);
 }
 
 void
@@ -242,40 +242,40 @@ SK_LCU_InitPlugin(void)
     SK_CreateFuncHook(L"ID3D11Device::CreateTexture2D",
         D3D11Dev_CreateTexture2D_Override,
         SK_LCU_CreateTexture2D,
-        (LPVOID*)&D3D11Dev_CreateTexture2D_Original);
+        (LPVOID*)&_D3D11Dev_CreateTexture2D_Original);
     MH_QueueEnableHook(D3D11Dev_CreateTexture2D_Override);
 
 
     SK_CreateFuncHook(L"ID3D11Device::CreateRenderTargetView",
         D3D11Dev_CreateRenderTargetView_Override,
         SK_LCU_CreateRenderTargetView,
-        (LPVOID*)&D3D11Dev_CreateRenderTargetView_Original);
+        (LPVOID*)&_D3D11Dev_CreateRenderTargetView_Original);
     MH_QueueEnableHook(D3D11Dev_CreateRenderTargetView_Override);
 
     SK_CreateFuncHook(L"ID3D11Device::CreateDepthStencilView",
         D3D11Dev_CreateDepthStencilView_Override,
         SK_LCU_CreateDepthStencilView,
-        (LPVOID*)&D3D11Dev_CreateDepthStencilView_Original);
+        (LPVOID*)&_D3D11Dev_CreateDepthStencilView_Original);
     MH_QueueEnableHook(D3D11Dev_CreateDepthStencilView_Override);
 
 
     SK_CreateFuncHook(L"ID3D11DeviceContext::OMSetRenderTargets",
         D3D11_OMSetRenderTargets_Override,
         SK_LCU_OMSetRenderTargets,
-        (LPVOID*)&D3D11_OMSetRenderTargets_Original);
+        (LPVOID*)&_D3D11_OMSetRenderTargets_Original);
     MH_QueueEnableHook(D3D11_OMSetRenderTargets_Override);
 
 
     SK_CreateFuncHook(L"ID3D11DeviceContext::RSSetViewports",
         D3D11_RSSetViewports_Override,
         SK_LCU_RSSetViewports,
-        (LPVOID*)&D3D11_RSSetViewports_Original);
+        (LPVOID*)&_D3D11_RSSetViewports_Original);
     MH_QueueEnableHook(D3D11_RSSetViewports_Override);
 
     SK_CreateFuncHook(L"ID3D11DeviceContext::RSSetScissorRects",
         D3D11_RSSetScissorRects_Override,
         SK_LCU_RSSetScissorRects,
-        (LPVOID*)&D3D11_RSSetScissorRects_Original);
+        (LPVOID*)&_D3D11_RSSetScissorRects_Original);
     MH_QueueEnableHook(D3D11_RSSetScissorRects_Override);
 }
 
